627faa4ec2195aa2255be75db1a218a268894f63
ChangG11 <gcmighty@gmail.com>
2025-07-10 19:58:29 +0000

    work on physics module until beginning of problem 8

---
+++ physics.py
@@ -0,0 +1,66 @@
+G = 9.81  # universal gravity constant, 9.81 m/s^2
+density_water = 1000 # density of water is 1000 kg/m^3
+mass_of_auv = 100 # defaults to 100 Kg
+volume_of_auv = 0.1 # defaults to 0.1 m^3
+thruster_distance = 0.5 # defaults to 0.5 m
+
+
+def calculate_buoyancy(
+    v, density_fluid
+):  # v should be iin m^3, density should be in kg/m^3, G is gravity = 9.81 m/s^2
+    """
+    This function calculates the buoyancy force based on the buoyancy force equation, p * V * g = Fb, and returns it
+    """
+    buoyancy_force = density_fluid * v * G
+    return buoyancy_force
+
+
+def will_it_float(v, mass):  # v in m^3, mass in kg
+    """
+    determines if an object will float in water by comparing the objects density to the density of water
+    """
+    density_of_object = mass / v
+    if density_of_object > 1000: #density of water is 1000 kg/m^3
+        return False
+    elif density_of_object < 1000:
+        return True
+    
+def calculate_pressure(depth): # p = density * gravity * depth
+    '''
+    calculates pressure in water based on the depth that the object is at using the equation pressure(Pa) = density(kg/m^3) * gravity(m/s^2) * depth(m), returns the pressure in pascals
+    '''
+    pressure = density_water * G * depth
+    return pressure # pressure should be returned in pascals
+
+def calculate_acceleration(F, m): # F is force applied to object in Newtons, m is the mass of object in Kg
+    '''
+    calculates acceleration based on newtons second law F = ma, but rewritten to solve for a. returns the acceleration in m/s^2. takes arguments F in Newtons, and mass in Kg
+    '''
+    acceleration = F/m
+    return acceleration
+
+def calculate_angular_acceleration(tau, I): # angular acceleration = torque (tau) / moment of inertia (I)
+    """ 
+    tau in Nm, I in kg*m^2, returns angular acceleration
+    """
+    angular_acceleration = tau / I
+    return angular_acceleration
+
+
+def calculate_torque(F_magnitude, r): # torque = distance from axis of rotation * force applied
+    '''
+    calculates torque with torque = distance from axis of rotation * force applied, returns the torque
+    '''
+    torque  = r * F_magnitude 
+    return torque
+
+
+def calculate_moment_of_inertia(m, r): 
+    '''
+    the equation for calculating the moment of inertia is given by the following equation for rigid bodies: moment of inertia = mass of object * (distance from axis of ratation to the center of mass of the object)^2
+    '''
+    moment_of_inertia = m * r^2
+    return moment_of_inertia
+
+def calculate_auv_acceleration(F_magnitude, F_angle):
+    return acceleration_of_auv
