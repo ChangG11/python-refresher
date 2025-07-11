db0e45a5c8f2e17f6db5bf18be84bec6b3f493e2
ChangG11 <gcmighty@gmail.com>
2025-07-11 03:55:46 +0000

    finished up to problem 10

--- physics.py
+++ physics.py
@@ -3,10 +3,10 @@ import numpy as np
 
 G = 9.81  # universal gravity constant, 9.81 m/s^2
 density_water = 1000 # density of water is 1000 kg/m^3
-mass_of_auv = 100 # defaults to 100 Kg
+# mass_of_auv = 100 # defaults to 100 Kg
 volume_of_auv = 0.1 # defaults to 0.1 m^3
-thruster_distance = 0.5 # defaults to 0.5 m
-inertia_of_AUV = 1 # defaults to 1 kg * m^2
+# thruster_distance = 0.5 # defaults to 0.5 m
+# inertia_of_AUV = 1 # defaults to 1 kg * m^2
 
 
 def calculate_buoyancy(
@@ -51,11 +51,12 @@ def calculate_angular_acceleration(tau, I): # angular acceleration = torque (tau
     return angular_acceleration
 
 
-def calculate_torque(F_magnitude, r): # torque = distance from axis of rotation * force applied
+def calculate_torque(F_magnitude, F_direction_deg, r): # torque = distance from axis of rotation * force applied * angle between  force and lever arm
     '''
     calculates torque with torque = distance from axis of rotation * force applied, returns the torque
     '''
-    torque  = r * F_magnitude 
+    F_direction = math.radians(F_direction_deg)
+    torque  = r * F_magnitude  * math.sin(F_direction)
     return torque
 
 
@@ -63,18 +64,38 @@ def calculate_moment_of_inertia(m, r):
     '''
     the equation for calculating the moment of inertia is given by the following equation for rigid bodies: moment of inertia = mass of object * (distance from axis of ratation to the center of mass of the object)^2
     '''
-    moment_of_inertia = m * r^2
+    moment_of_inertia = m * r**2
     return moment_of_inertia
 
-def calculate_auv_acceleration(F_magnitude, F_angle):
-    F_x = F_magnitude * np.cos(F_angle)
-    F_y = F_magnitude * np.sin(F_angle)
+def calculate_auv_acceleration(F_magnitude, F_angle, mass_of_auv):
+    F_x = F_magnitude * math.cos(F_angle)
+    F_y = F_magnitude * math.sin(F_angle)
     a_x = F_x / mass_of_auv
     a_y = F_y / mass_of_auv
-    acceleration_of_auv = np.sqrt(a_x**2 + a_y**2)
-    return acceleration_of_auv
+    return (a_x, a_y)
 
-def calculate_auv_angular_acceleration(F_magnitude, F_angle): # angaccel = torque / moment of inertia, torque = distance from axis of rotation * force applied, moment of inertia = mass * distance ^2
-    torque = thruster_distance * calculate_auv_acceleration(F_magnitude, F_angle)
-    angular_acceleration = torque / inertia_of_AUV
+def calculate_auv_angular_acceleration(F_magnitude, F_angle, inertia = 1.0, thruster_distance = 0.5): # angaccel = torque / moment of inertia, torque = distance from axis of rotation * force applied, moment of inertia = mass * distance ^2
+    torque = thruster_distance * F_magnitude * math.sin(F_angle)
+    angular_acceleration = torque / inertia
     return angular_acceleration
+
+
+def calculate_auv2_acceleration(T, alpha, theta, mass = 100.0):
+    cos_alpha = math.cos(alpha)
+    sin_alpha = math.sin(alpha)
+
+    F_x = (-T[0] + T[1] + T[2] - T[3]) * cos_alpha
+    F_y = (T[0] + T[1] - T[2] - T[4]) * sin_alpha
+
+    a_x = F_x / mass
+    a_y = F_y / mass
+    return (a_x, a_y)
+
+def calculate_auv2_angular_acceleration(T, alpha, L, l, inertia):
+    torque_factor = (L * math.sin(alpha)) + (l * math.cos(alpha))
+    toruqe = (T[0] + T[1] + T[2] + T[3]) * torque_factor
+    angular_acceleration = toruqe / inertia
+    return angular_acceleration
+
+def simulate_auv2_motion(T, alpha, L, l, mass = 100.0, inertia = 100.0, dt = .1, t_final = 10.0, x0 = 0.0, y0 = 0.0, theta0 = 0.0):
+    pass
